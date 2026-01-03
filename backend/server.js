c98debf013ca1b7f9d795ea86df9ce18a5269080
sebi100230 <sebigaj@icloud.com>
2026-01-03 11:57:22 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
-import { isLegalContent } from "./filters.js";
+import { analyzeContent } from "./filters.js";
 
 const app = express();
 app.use(cors());
